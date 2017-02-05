f5d8452f464a043fb79dd287a051a365c56e319c
skmisu <satyajitkaroo7@gmail.com>
2017-02-05 12:33:56 +0600

    change in user adapter and mainactivity.class

--- app/src/main/java/com/dhakadigital/tdd/wtc/activity/MainActivity.java
+++ app/src/main/java/com/dhakadigital/tdd/wtc/activity/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -79,6 +80,10 @@ EarningInfo earningInfo = new EarningInfo("00","00","00","00");
 
         //set up adapter
         earningInfoAdapter = new EarningInfoAdapter(earningInfos);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        layoutManager.setAutoMeasureEnabled(true);
+        rvEarnList.setLayoutManager(layoutManager);
+        rvEarnList.setAdapter(earningInfoAdapter);
 
     }
 
